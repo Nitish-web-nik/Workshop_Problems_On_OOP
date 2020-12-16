5a2cde21e8a1be14d12d6df5c72c8cbb337d640c
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2020-12-16 23:47:53 +0530

    Add files via upload

---
+++ Day 10 References/bad_reference.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *p = nullptr;
+    int& r = *p;
+    r = '!';
+    return 0;
+}
---
+++ Day 10 References/pointers_as_function_arguments.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void copychar(char *from, char* to, int count)
+{
+    if(from == nullptr || to == nullptr)
+    {
+        return;
+    }
+    while(count-- > 0)
+    {
+        *to++ = *from++;
+    }
+}
+int main()
+{
+    char string[] = {"nitish kumar"};
+    char buffer[15];
+    copychar(string, buffer,13);
+    cout<<buffer;
+    return 0;
+}
