4076f2b8a98d8e9dc99ba788066c7e79cbedfbba
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-07 13:20:22 +0530

    Add files via upload

---
+++ Day-13 (Ownership and Lifetime of Dynamic Variables)/using_make_shared().cpp
@@ -0,0 +1,23 @@
+/*This exercise constructs a few dynamic variables using make_shared():*/
+#include <iostream>
+#include <memory>
+using namespace std;
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+    ~noisy() { cout << "destroying noisy" << endl; }
+};
+int main()
+{
+    shared_ptr<noisy> u1(new noisy);
+    shared_ptr<noisy> u2 = make_shared<noisy>();
+    shared_ptr<noisy> u3 = u2;
+    u2.reset();
+    return 0;
+}
+/*Output:-
+constructing noisy
+constructing noisy
+destroying noisy
+destroying noisy
+*/
