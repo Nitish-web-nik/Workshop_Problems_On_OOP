86161c27f7e44161e2a86db24bb3747534bf528c
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-07 14:52:54 +0530

    Add files via upload

---
+++ Day-14(Classes and Structs)/parameterized_constructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Track
+{
+public:
+    float m_length_in_sec;
+    string m_track_name;
+    Track(float track_length_in_sec, string track_name)
+    {
+        m_length_in_sec = track_length_in_sec;
+        m_track_name = track_name;
+    }
+};
+int main()
+{
+    Track track(200.0f, "Chip Thrills");
+    cout<<"Track name is "<<track.m_track_name<<endl;
+    cout<<"Track's length is "<<track.m_length_in_sec<<" seconds"<<endl;
+    return 0;
+}
+/*Output:-
+Track name is Chip Thrills
+Track's length is 200 seconds
+*/
