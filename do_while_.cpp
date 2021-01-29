bb420c145fb8b668927d8a2ac3dda95924b33ed4
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-29 08:48:56 +0530

    Update do_while_.cpp

--- do_while_.cpp
+++ do_while_.cpp
@@ -8,5 +8,7 @@ int main()
         cout <<a<<" ";
         a = a + 2;
 
-    }while(a<100);
+    }while(a<=100);
 }
+//Output:- 2 4 ------------- 100
+//If we initialize a with 1 then output will print all odd numbers from 1 to 99
