2e2d76de2928a137295eb87fd5033b63a0c05995
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-29 07:22:29 +0530

    Add files via upload

---
+++ while_loop_.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 0;
+    while(a<= 100)
+    {
+        //a++ Output: 0 to 101
+        cout<<a<<" ";
+        a++;//Output: 0 to 100
+    }
+    return 0;
+}
