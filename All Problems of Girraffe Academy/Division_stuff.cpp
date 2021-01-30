954ff057d16d35704731460dc908ee7342a58295
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-30 19:12:13 +0530

    Add files via upload

---
+++ All Problems of Girraffe Academy/Division_stuff.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<10 / 3<<endl;//3
+    cout<<10.0 / 3.0<<endl;//3.333
+    cout<<10.0 / 3<<endl;//3.333
+    cout<<10 / 3.0<<endl;//3.333
+    return 0;
+}
