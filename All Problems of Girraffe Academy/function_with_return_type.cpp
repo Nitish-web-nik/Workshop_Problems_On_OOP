e4e4bddddbd04fa3d30c545cbf932556ae08b2ed
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-30 20:38:57 +0530

    Add files via upload

---
+++ All Problems of Girraffe Academy/function_with_return_type.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+double cube(double);//function prototype/declaration
+int main()
+{
+    cout<<cube(3.4);//function call
+    return 0;
+}
+double cube(double num)//function definition
+{
+    return num * num * num;
+    cout<<"Note:- This is will not printed on console";
+}
+/* Output:- 39.304   */
