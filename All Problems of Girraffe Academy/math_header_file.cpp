94fa7332471d4a944addecbe6449610b18648aaa
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-30 19:12:50 +0530

    Add files via upload

---
+++ All Problems of Girraffe Academy/math_header_file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<pow(3,4)<<endl;//81
+    cout<<sqrt(25)<<endl;//5
+    cout<<sqrt(34)<<endl;//5.83095
+    cout<<round(4.4)<<endl;//4
+    cout<<round(4.5)<<endl;//5
+    cout<<ceil(4.3)<<endl;//5
+    cout<<floor(4.3)<<endl;//4
+    cout<<fmin(3, 10)<<endl;//3
+    cout<<fmax(3, 10)<<endl;//10
+    return 0;
+}
