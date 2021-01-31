02478c3f5ba50d8f3eea06a9d8401dff6ba959a9
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-31 13:12:17 +0530

    added an extra rule when defining default argument

--- easytt/func_with_defalut_arguments.cpp
+++ easytt/func_with_defalut_arguments.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int add(int a = 5, int b = 3, int c = 2)//Here a,b and c are default arguments
+/* NOTE:- Default arguments are given only from right to left i.e 
+int add(int a = 5, b, c) ---> Invalid 
+int add(int a = 5, b = 7, c) ---> Invalid
+int add(int a, int b, int c = 3) ---> Valid
+*/
 {
     return (a + b + c);
 }
