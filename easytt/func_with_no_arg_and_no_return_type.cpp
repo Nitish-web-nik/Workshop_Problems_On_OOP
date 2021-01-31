c121b5d2b51812874e5c20303297128383dc11f6
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-31 12:43:12 +0530

    Add files via upload

---
+++ easytt/func_with_no_arg_and_no_return_type.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void disp(void);
+int main()
+{
+    disp();
+    return 0;
+}
+void disp(void)
+{
+    cout<<"This function is without return type and and arguments"<<endl;
+}
