b02f2486471a0f51bdb7315488630690eddb77e3
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-01-31 13:39:59 +0530

    Add files via upload

---
+++ easytt/call_by_address_swap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swap(int*, int*);
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a:- ";
+    cin>>a;
+    cout<<"Enter the value of b:- ";
+    cin>>b;
+    cout<<"Before swapping a = "<<a<<" b = "<<b<<endl;
+    swap(&a,&b);
+    return 0;
+}
+void swap(int *p, int *q)
+{
+    cout<<"After swapping a = "<<*q <<" b = "<<*p<<endl;
+}
