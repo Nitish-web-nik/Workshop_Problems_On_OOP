5a68695f38b7ec28e831aaeaa82d92a7b5c8eede
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-02-01 12:18:22 +0530

    Update static_data_member.cpp

--- easytt/static_data_member.cpp
+++ easytt/static_data_member.cpp
@@ -34,6 +34,96 @@ int main()
     t3.showdata();
     return 0;
 }
+/*or 
+
+#include<iostream>
+using namespace std;
+
+class Test
+{
+    int no;
+    static int count;
+public:
+    void getdata(int);//if we don't declare as a void return type then by default it becomes of int type
+    void showdata(void);
+};
+void Test::getdata(int x)
+{
+    no = x;
+    cout<<"Number = "<<no<<endl;
+    count++;
+}
+void Test::showdata()
+{
+    cout<<"Count = "<<count<<endl;
+}
+int Test::count;
+int main()
+{
+    Test t1,t2,t3;
+    t1.showdata();
+    t2.showdata();
+    t3.showdata();
+    t1.getdata(5);
+    t2.getdata(4);
+    t3.getdata(3);
+    t1.showdata();
+    t2.showdata();
+    t3.showdata();
+    return 0;
+}
+/*Output:-
+#include<iostream>
+using namespace std;
+
+class Test
+{
+    int no;
+    static int count;
+public:
+    void getdata(int);
+    void showdata(void);
+};
+void Test::getdata(int x)
+{
+    no = x;
+    cout<<"Number = "<<no<<endl;
+    count++;
+}
+void Test::showdata()
+{
+    cout<<"Count = "<<count<<endl;
+}
+int Test::count;
+int main()
+{
+    Test t1,t2,t3;
+    t1.showdata();
+    t2.showdata();
+    t3.showdata();
+    t1.getdata(5);
+    t2.getdata(4);
+    t3.getdata(3);
+    t1.showdata();
+    t2.showdata();
+    t3.showdata();
+    return 0;
+}
+/*Output:-
+Count = 0
+Count = 0
+Count = 0
+Number = 5
+Number = 4
+Number = 3
+Count = 3
+Count = 3
+Count = 3
+*/
+
+*/
+
+*/
 /*Output:-
 Count = 0
 Count = 0
