a594494f945625abd2961b86ce9a79adddf45728
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-02-02 09:38:55 +0530

    default_constructor

---
+++ easytt/default_constructor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Abc
+{
+    int a,b;
+public:
+    Abc()
+    {
+        a = 5;
+        b = 74;
+    }
+    void disp(void)
+    {
+        cout<<"The value of A is "<<a<<endl;
+        cout<<"The value of B is "<<b<<endl;
+    }
+};
+int main()
+{
+    Abc t;
+    t.disp();
+    return 0;
+}
+/*Output:-
+The value of A is 5
+The value of B is 74
+*/
