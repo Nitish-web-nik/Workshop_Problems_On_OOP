d1e0f5b06bae521a9e8ef2857b743272a7573325
nitish_web_nik <57797801+Nitish-web-nik@users.noreply.github.com>
2021-02-02 09:40:34 +0530

    default_copy_constructor

---
+++ easytt/default_copy_constructor.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Abc
+{
+    int a,b;
+public:
+    Abc()
+    {
+        a = 5;
+        b = 74;
+    }
+    void disp(void)
+    {
+        cout<<"The value of A is "<<a<<endl;
+        cout<<"The value of B is "<<b<<endl;
+    }
+};
+int main()
+{
+    Abc t,t1;
+    t.disp();
+    cout<<"Value after Default Copy Constructor"<<endl;
+    t1 = t;
+    t1.disp();
+    return 0;
+}
+/*Output:-
+The value of A is 5
+The value of B is 74
+Value after Default Copy Constructor
+The value of A is 5
+The value of B is 74
+*/
