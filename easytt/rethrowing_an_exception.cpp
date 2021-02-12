c418aa8cfd508af147b6b16f52f50a1d3a2d28b3
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-02-12 13:04:48 +0530

    rethrowing_an_exception

---
+++ easytt/rethrowing_an_exception.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+void function(int x, int y)
+{
+    cout<<"Inside function"<<endl;
+    try
+    {
+        if(y == 0)
+        {
+            throw y;
+        }
+        else
+        {
+            cout<<"Division:- "<<(x/y)<<endl;
+        }
+    }
+    catch(int i)
+    {
+        cout<<"Caught Integer inside function"<<endl;
+        throw;
+    }
+    cout<<"End of function"<<endl;
+}
+int main()
+{
+    cout<<"Inside main Function"<<endl;
+    try
+    {
+        function(10,5);
+        cout<<endl;
+        function(10,0);
+
+    }
+    catch(int no)
+    {
+        cout<<"Caught integer inside Main "<<no<<endl;
+    }
+    cout<<"End of Main Function"<<endl<<endl;
+    return 0;
+}
+/*Output:-
+
+Inside main Function
+Inside function
+Division:- 2
+End of function
+
+Inside function
+Caught Integer inside function
+Caught integer inside Main 0
+End of Main Function
+*/
